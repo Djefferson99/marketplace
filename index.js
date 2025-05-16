8387deb607fcc19f253708e364f8d098ca7d0968
Djeff <djeff.walla99@gmail.com>
2025-05-15 23:06:00 -0300

    Corrigir porta para process.env.PORT

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
 const usuarioRoutes = require('./src/routes/usuarioRoutes');
 const servicoRoutes = require('./src/routes/servicoRoutes');
 const agendamentoRoutes = require('./src/routes/agendamentoRoutes');
 
-app.use(express.json());
 app.use('/usuarios', usuarioRoutes);
 app.use('/servicos', servicoRoutes);
 app.use('/agendamentos', agendamentoRoutes);
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
