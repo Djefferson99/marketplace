4955b99676e6953f77daab145d221b2a7bf7c40f
Djeff <djeff.walla99@gmail.com>
2025-05-22 14:06:20 -0300

    autentificacao

--- src/routes/empresaRoutes.js
+++ src/routes/empresaRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const empresaController = require('../controllers/empresaController');
+const autenticarToken = require('../middlewares/authMiddleware');
 
-router.post('/', empresaController.create);
+router.post('/',autenticarToken, empresaController.create);
 router.get('/', empresaController.getAll);
 router.get('/:id', empresaController.getById);
-router.put('/:id', empresaController.update);
-router.delete('/:id', empresaController.delete);
+router.put('/:id',autenticarToken, empresaController.update);
+router.delete('/:id',autenticarToken, empresaController.delete);
 
 module.exports = router;
--- src/routes/servicoRoutes.js
+++ src/routes/servicoRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const servicoController = require('../controllers/servicoController');
+const autenticarToken = require('../middlewares/authMiddleware');
 
-router.post('/', servicoController.create);
+router.post('/',autenticarToken, servicoController.create);
 router.get('/', servicoController.getAll);
 router.get('/:id', servicoController.getById);
-router.put('/:id', servicoController.update);
-router.delete('/:id', servicoController.delete);
+router.put('/:id',autenticarToken, servicoController.update);
+router.delete('/:id',autenticarToken, servicoController.delete);
 
 module.exports = router;
