ab5b3210cde8c3ee9ffd14ac45dd8f2b2c0e5cfe
Djeff <djeff.walla99@gmail.com>
2025-06-02 14:58:01 -0300

    back

--- src/routes/servicoRoutes.js
+++ src/routes/servicoRoutes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const servicoController = require('../controllers/servicoController');
 const autenticarToken = require('../middlewares/authMiddleware');
 
-router.post('/',autenticarToken, servicoController.create);
-router.get('/', servicoController.getAll);
-router.get('/:id', servicoController.getById);
-router.put('/:id',autenticarToken, servicoController.update);
-router.delete('/:id',autenticarToken, servicoController.delete);
+router.post('/servicos',autenticarToken, servicoController.create);
+router.get('/servicos', servicoController.getAll);
+router.get('/servicos/:id', servicoController.getById);
+router.put('/servicos/:id',autenticarToken, servicoController.update);
+router.delete('/servicos/:id',autenticarToken, servicoController.delete);
 
 module.exports = router;
