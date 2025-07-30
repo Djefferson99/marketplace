587374a437aa1b9bff25542d20e1c25aac3ed685
Djeff <djeff.walla99@gmail.com>
2025-07-29 22:00:54 -0300

    volta3

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const authRoutes = require('./src/routes/authRoutes');
 const empresaRoutes = require('./src/routes/empresaRoutes');
 
 app.use(cors({
-  origin: ['www.indca.com.br'],
+  origin: ['https://www.indca.com.br'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
