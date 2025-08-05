b430f612a293f20c2535ed587211831442025dfc
Djeff <djeff.walla99@gmail.com>
2025-08-05 13:55:46 -0300

    img up

--- src/routes/empresaRoutes.js
+++ src/routes/empresaRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const empresaController = require('../controllers/empresaController');
+// a seguir, certifique-se que o caminho bate com onde você criou:
+const upload = require('../middlewares/upload');  
 
-router.post('/', empresaController.create);
+router.post('/', upload.single('foto_perfil'), empresaController.create);
 router.get('/', empresaController.getAll);
 router.get('/:id', empresaController.getById);
 router.get('/usuario/:usuario_id', empresaController.getByUsuarioId);
-router.put('/:id', empresaController.update);
+router.put('/:id', upload.single('foto_perfil'), empresaController.update);
 router.delete('/:id', empresaController.delete);
 
 module.exports = router;
